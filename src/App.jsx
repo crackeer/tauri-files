a6783afd2a0394a21345bbe17c5e9ad2b8d21ef6
liuhu016 <liuhu016@realsee.com>
2025-02-17 07:47:14 +0800

    feat:add rust

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
 import React, { useEffect, useState } from "react";
 import "jsoneditor/dist/jsoneditor.css";
 import "@arco-design/web-react/dist/css/arco.css";
-import { Button, Input, Space, Message, Tabs, Table, Form, Modal } from "@arco-design/web-react";
+import { Button, Input, Space, Message, Tabs, Table, Form, Modal,  Tooltip, Typography } from "@arco-design/web-react";
 import { writeTextFile, BaseDirectory, readTextFile, exists, create, mkdir } from '@tauri-apps/plugin-fs';
-import { IconSave, IconImport, IconFire, IconAlignLeft, IconLaunch, IconStop, IconCopy } from "@arco-design/web-react/icon";
+import { IconSave, IconImport, IconFire, IconAlignLeft, IconLaunch, IconStop, IconCopy, IconDelete } from "@arco-design/web-react/icon";
 import dayjs from "dayjs";
 import { save, open } from '@tauri-apps/plugin-dialog';
-import { openPath, openUrl } from '@tauri-apps/plugin-opener';
-import {open as shellOpen} from '@tauri-apps/plugin-shell'
 import invoke from "@/util/invoke";
 import file from "@/util/file";
 import { writeText } from '@tauri-apps/plugin-clipboard-manager';
 const TabPane = Tabs.TabPane;
 const FormItem = Form.Item;
+const { Text } = Typography;
 
 
-
-var editor = null;
 const formItemLayout = {
     labelCol: {
         span: 5,
@@ -37,30 +34,66 @@ function App1() {
         {
             title: '名字',
             dataIndex: 'name',
+            render: (text, record) => {
+                if (record.name.length < 30) {
+                    return text
+                }
+                return <Tooltip content={record.name}>
+                    <Text>{text.substr(0, 27) + '...'}</Text>
+                </Tooltip>
+            },
+            sorter: (a, b) => {
+                if (a.name > b.name)  {
+                    return 1
+                } else if (a.name < b.name) {
+                    return -1
+                } else {
+                    return 0
+                }
+            },
         },
         {
             title: '大小',
+            sorter: (a, b) => a.size - b.size,
             dataIndex: 'human_size',
         },
         {
             title: '创建时间',
+            sorter: (a, b) => {
+                if (a.create_at > b.create_at)  {
+                    return 1
+                } else if (a.create_at < b.create_at) {
+                    return -1
+                } else {
+                    return 0
+                }
+            },
             dataIndex: 'create_at',
         },
         {
             title: '修改时间',
             dataIndex: 'modify_at',
+            sorter: (a, b) => {
+                if (a.modify_at > b.modify_at)  {
+                    return 1
+                } else if (a.modify_at < b.modify_at) {
+                    return -1
+                } else {
+                    return 0
+                }
+            },
         },
         {
             title: '操作',
             dataIndex: 'action',
             render: (text, record) => {
                 return <Space size="mini">
-                    <Button type='primary' status="danger" size='mini' onClick={toDelete.bind(this, record)}>
-                        删除
+                    <Button type="primary" status="primary"  icon={<IconCopy />} size='mini' onClick={copy.bind(this, record.path)}></Button>
+                    <Button type='primary' status="danger" size='mini' onClick={toDelete.bind(this, record)}  icon={<IconDelete />} >
                     </Button>
                     <Button type='primary' status="info" size='mini' onClick={async () => {
                         invoke.openPath(record.path) 
-                    }}>打开</Button>
+                    }} icon={<IconLaunch />}></Button>
                 </Space>
             }
         }
@@ -98,8 +131,7 @@ function App1() {
     }, [currentDir])
 
 
-    var copy = () => {
-        let text = editor.getText()
+    var copy = (text) => {
         writeText(text).then(() => {
             Message.success("复制成功")
         })
@@ -145,6 +177,7 @@ function App1() {
         let result = await file.setAllDirs(dirs)
         Message.success("添加成功")
         setAllDirs(dirs)
+        setVisible(false)
     }
 
     var toSelectDir = async () => {
@@ -213,7 +246,7 @@ function App1() {
                         <div style={{ paddingLeft: '10px', paddingBottom: '10px' }}>{item.path} <Space>
 
                             <Button type='outline' size="mini" icon={<IconLaunch />}  onClick={toDir.bind(this, index, item)}></Button>
-                            <Button type='outline' size="mini" icon={<IconCopy />}></Button>
+                            <Button type='outline' size="mini" icon={<IconCopy />} onClick={copy.bind(this, item.path)}></Button>
                             <Button type='outline' size="mini" status="danger" icon={<IconStop />} onClick={cancelDir.bind(this, index, item)}></Button>
                         </Space> </div>
                     </TabPane>
@@ -221,7 +254,7 @@ function App1() {
             }
         </Tabs>
 
-        <Table columns={columns} data={files} pagination={false} border={false} loading={loading} />
+        <Table columns={columns} data={files} pagination={false} border={false} loading={loading} defaultSortOrder={'descend'}/>
 
         <Modal
             title='添加文件夹'
